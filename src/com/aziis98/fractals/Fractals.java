98343ac63aa452d0378ac7a3b243d3a4b4fa84ca
aziis98 <antonio.delucreziis@gmail.com>
2015-10-07 15:59:59 +0200

    Second commit ever

--- src/com/aziis98/fractals/Fractals.java
+++ src/com/aziis98/fractals/Fractals.java
@@ -1,9 +1,11 @@
 package com.aziis98.fractals;
 
+import com.aziis98.dare.system.*;
+
 public class Fractals {
 
     public static void main(String[] args) {
-        System.out.println( "Hello, World!" );
+        Console.out( "Hello, World! by CoreAPI" );
     }
 
 }
